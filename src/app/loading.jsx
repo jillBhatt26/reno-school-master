b72f28828a669783e3f3283d2d409098d886e225
jill bhatt <bhattjill@gmail.com>
2025-09-03 02:35:51 +0530

    Added custom loader page

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return (
+        <div className="w-screen h-96 flex justify-center items-center">
+            <span className="loading loading-bars loading-xl my-auto"></span>
+        </div>
+    );
+};
+
+export default Loader;
